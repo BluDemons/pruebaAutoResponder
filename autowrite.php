3f7820e531a71f979cd28975dbe8a1c297be30e3
BluDemons <jar.salazar@yavirac.edu.ec>
2019-10-17 11:15:16 -0500

    autoresponder

---
+++ autowrite.php
@@ -0,0 +1,14 @@
+<?php
+$autosubject="Contact Form Auto Responser";
+$name="ajax2.zip";
+$type="application/zip";
+$autofrom="jar.salazar@yavirac.edu.ec";
+$automessage="This is an Contact form auto responder
+
+Here your message goes
+
+
+Regards
+Admin";
+$attachment="/home/public_html/upload/ajax2.zip";
+?>
